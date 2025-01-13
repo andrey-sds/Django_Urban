b9b015ad19aec3ca33ec1a9d1592edf2871e2bf6
andrey-sds <dz_andrey@mail.ru>
2025-01-13 20:40:03 +0300

    Add files via upload

--- Module_18/UrbanDjango/UrbanDjango/urls.py
+++ Module_18/UrbanDjango/UrbanDjango/urls.py
@@ -22,9 +22,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('task4.urls', namespace='task4')),
+    path('task4', include('task4.urls', namespace='task4')),
     path('task3', include('task3.urls', namespace='task3')),
     path('lesson2/', include('task2.urls', namespace='task2')),
     path('django_sign_up/', include('task5.urls', namespace='task5')),
-    path('django/', include('task5.urls', namespace='task5')),
+    path('', include('task5.urls', namespace='task5')),
 ]
